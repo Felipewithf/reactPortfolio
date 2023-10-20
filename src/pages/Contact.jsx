5f58696628fd65efde344b2732c77307ec2a9fc9
Felipe <felipe.sarmiento.as@gmail.com>
2023-10-20 11:57:40 -0400

    completed form in contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,20 +1,93 @@
+
+import { useState } from 'react';
+
 export default function Contact() {
+
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [message, setMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
+
+const handleChange = (event) => {
+  
+  setErrorMessage('');
+  const { target } = event;
+  const inputType = target.name;
+  const inputValue = target.value;
+
+
+  switch (inputType) {
+    case 'name':
+      setName(inputValue);
+      break;
+      case 'email':
+        setEmail(inputValue);
+      break;
+      case 'message':
+        setMessage(inputValue);
+      break;
+    default:
+      break;
+  }
+}
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  if (!message || !name || !email) {
+    setErrorMessage('Please fill all the fields');
+    return;
+  }
+  // clear out the input after a successful registration.
+  setName('');
+  setMessage('');
+  setEmail('');
+
+  setSuccessMessage('Message Delivered!');
+
+  setTimeout(()=>{
+    setSuccessMessage('');
+}, 2000)
+
+  
+};
+
   return (
-    <div>
-      <h1>Contact Page</h1>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-      </p>
-    </div>
+    <section>
+      <h1>Contact</h1>
+      <form onSubmit={handleSubmit}>
+      <div className="form-group py-3">
+    <label for="InputName">Name</label>
+    <input type="text" className="form-control" id="InputName" name="name" onChange={handleChange} value={name} placeholder=""/>
+  </div>
+  <div className="form-group py-3">
+    <label for="InputEmail">Email address</label>
+    <input type="email" className="form-control" id="InputEmail" name="email" onChange={handleChange} value={email} aria-describedby="emailHelp" placeholder=""/>
+    <small id="emailHelp" className="form-text text-muted">Will never share your email with anyone else.</small>
+  </div>
+  <div className="form-group py-3">
+  <label for="message">Message</label>
+    <textarea className="form-control" id="message" name="message" onChange={handleChange} value={message} rows="5"></textarea>
+  </div>
+
+  {errorMessage && (
+        <div>
+          <p className="alert alert-danger">{errorMessage}</p>
+        </div>
+      )}
+ 
+  {successMessage && (
+        <div>
+          <p className="alert alert-success">{successMessage}</p>
+        </div>
+      )}
+
+  <button type="submit" className="btn btn-primary mt-5">Submit</button>
+</form>
+
+
+
+    </section>
   );
 }
